6b07efb5b504e8581fee9497f56341135614d647
AakashThakur23102000 <aakashthakur20001972@gmail.com>
2022-09-16 00:02:37 +0530

    Signed-off-by: AakashThakur23102000 <aakashthakur20001972@gmail.com>

---
+++ CODES/3 Variables for Numbers.js
@@ -0,0 +1,20 @@
+/*You can also assign a number like this  */
+
+var weights = 150;
+alert(weights);
+
+/* add in weight */
+alert(weights + 25);
+
+/*Assigning new vairable to the sum*/
+var oldweight = 40;
+var newweight = oldweight + 5;
+alert(newweight)
+
+/*A variable can be used in calculating its own new value. */
+var originalNum = 90;
+originalNum = originalNum + 10;
+alert(originalNum);
+
+/* you can directly print the values wighout variable */
+alert(155);
