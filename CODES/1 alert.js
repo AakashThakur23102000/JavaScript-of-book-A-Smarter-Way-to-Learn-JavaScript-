78da3d4b04546617927d8520ce9cea95c35834ca
AakashThakur23102000 <aakashthakur20001972@gmail.com>
2022-09-15 23:11:19 +0530

    Signed-off-by: AakashThakur23102000 <aakashthakur20001972@gmail.com>

---
+++ CODES/1 alert.js
@@ -0,0 +1,8 @@
+/* 1
+Alerts
+An alert is a box that pops up to give the user a message. Here's code for an alert that
+displays the message "Thanks for your input!"
+ */
+
+
+alert("Thanks for your input!");
---
+++ CODES/2 Variables for String.js
@@ -0,0 +1,13 @@
+/* This is a JavaScript statement that creates the variable name and assigns the value "Aakash" to it. */
+var name = "Aakash";
+alert(name);
+
+/*After you declare a variable one time you can change that variable wighout using var keyword.*/
+name = "Ram Ram"
+alert(name); 
+
+/*You can declare a variable in one statement, leaving it undefined. Then you can assign a
+value to it in a later statement, without declaring it again.*/
+var states;
+states = "DELHI";
+alert(states);
